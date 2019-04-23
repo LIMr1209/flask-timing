a5d1eb3f0213de3c5b2488e8d5dfcb36b0832586
LIMr1209 <tianshuaiorc@sina.com>
2019-04-23 11:01:16 +0800

    delete

--- celery_tasks/task2.py
+++
@@ -1,8 +0,0 @@
-import time
-
-from celery_tasks.main_task import celery2
-
-@celery2.task
-def t2(id):
-    print("当前时间：", time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time())))
-    print("我是task2方法2","id:"+id)
